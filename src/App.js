1add80b51bbc488220f733cd760cba3f0f81e77f
ZohaibFattani <fattanizohaib@gmail.com>
2022-10-27 18:32:03 -0700

    added bootstrap styles to components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 
-import Home from './components/Home'
-import About from './components/About'
-import Packages from './components/Packages'
-
+import Home from "./components/Home";
+import About from "./components/About";
+import Packages from "./components/Packages";
 
 function App() {
-  
-  const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath']
-
+  const packages = [
+    "Activate your Crystals",
+    "Monkey Meditation",
+    "Soak in the Hotsprings",
+    "Hypnotherapy",
+    "Mineral Bath",
+  ];
 
   return (
     <div className="App">
@@ -19,35 +24,39 @@ function App() {
           <h1 className="title">Welcome to Monty's Mineral SPA</h1>
 
           <div className="navBar">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About Us</Link>
-              </li>
-              <li>
-                <Link to="/packages">Our Packages</Link>
-              </li>
-            </ul>
+            <Container>
+              <Nav defaultActiveKey="/" variant="tabs" fill>
+                <Nav.Item>
+                  <Nav.Link href="/">
+                    <Link to="/">Home</Link>
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey={"aboutPage"}>
+                    <Link to="/about">About Us</Link>
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey={"packagesPage"}>
+                    <Link to="/packages">Our Packages</Link>
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Container>
           </div>
         </header>
 
         <div className="display">
           <Route path="/" component={Home} />
           <Route path="/about" component={About} />
-          <Route path="/packages" render={() => <Packages packages={packages}/>}  />
+          <Route
+            path="/packages"
+            render={() => <Packages packages={packages} />}
+          />
         </div>
-
       </Router>
-
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
